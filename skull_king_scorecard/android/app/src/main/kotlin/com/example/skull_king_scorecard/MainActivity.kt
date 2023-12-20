527f2aa2777dff9949f34c10e8a9d1151352fe4c
91jaeminjo <91jaeminjo@gmail.com>
2023-12-19 22:29:58 -0600

    add new flutter project

---
+++ skull_king_scorecard/android/app/src/main/kotlin/com/example/skull_king_scorecard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.skull_king_scorecard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ skull_king_scorecard/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SkullKingScorecard());
+}
+
+class SkullKingScorecard extends StatelessWidget {
+  const SkullKingScorecard({super.key});
+
+  static const title = 'Skull King Scorecard';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: title),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            OutlinedButton(onPressed: () {}, child: const Text('New Game')),
+          ],
+        ),
+      ),
+    );
+  }
+}
